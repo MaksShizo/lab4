2c6c3334175413977ef8942afda84da6bc8cce68
maksd <maksdi3@yandex.ru>
2024-04-07 16:53:29 +0500

    fix finally

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,22 +1,22 @@
 ﻿#include <iostream>
 #include <mpi.h>
+#include <chrono>
 
-const int N = 4; // Размер матрицы и вектора
+const int N = 100;
+std::chrono::high_resolution_clock::time_point startTime, finishTime;
 
-void printMatrix(double* A, int rows, int cols) {
+double A[N][N];
+double B[N][N];
+double C[N][N];
+double D[N][N];
+
+void printMatrix(double A[N][N], int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            std::cout << A[i * cols + j] << " ";
+            std::cout << A[i][j] << " ";
         }
         std::cout << std::endl;
     }
-}
-
-void printVector(double* V, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << V[i] << " ";
-    }
-    std::cout << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -25,57 +25,44 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int blockSize = N / size; // Размер блока для каждого процесса
-
-    double* A = new double[N * N];
-    double* b = new double[N];
-    double* x = nullptr;
-
     if (rank == 0) {
-        A = new double[N * N] {
-            4, -2, 1, -1,
-                -2, 4, -2, 1,
-                1, -2, 4, -2,
-                -1, 1, -2, 4
-            };
-
-        b = new double[N] {11, -16, 17, -5};
-    }
-
-    // Распределение данных
-    double* localA = new double[blockSize * N];
-    double* localB = new double[blockSize];
-
-    MPI_Bcast(A, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(b, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatter(A, blockSize * N, MPI_DOUBLE, localA, blockSize * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatter(b, blockSize, MPI_DOUBLE, localB, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    // Решение локальной системы
-    for (int k = 0; k < N; ++k) {
-        for (int i = 0; i < blockSize; ++i) {
-            double scale = localA[i * N + k] / A[k * N + k];
-            for (int j = k + 1; j < N; ++j) {
-                localA[i * N + j] -= scale * A[k * N + j];
+        startTime = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                A[i][j] = i + 1;
+                B[i][j] = j + 1;
+                C[i][j] = 0;
+                D[i][j] = 0;
             }
-            localB[i] -= scale * b[k];
         }
     }
 
-    // Объединение результатов
-    MPI_Gather(localB, blockSize, MPI_DOUBLE, x, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(A, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(B, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Вывод результата на 0-м процессе
-    if (rank == 0) {
-        std::cout << "Solution: ";
-        printVector(x, N);
+    int blockSize = N / size;
+    int remainder = N % size;
+    int start = rank * blockSize;
+    int end = start + blockSize;
+
+    if (rank == size - 1) {
+        end += remainder;
     }
 
-    delete[] localA;
-    delete[] localB;
-    delete[] A;
-    delete[] b;
-    delete[] x;
+    for (int i = start; i < end; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < N; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    MPI_Reduce(C, D, N * N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        std::cout << "Resulting Matrix:" << std::endl;
+        printMatrix(D, N, N);
+    }
 
     MPI_Finalize();
     return 0;
